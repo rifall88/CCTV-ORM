a99e956bd3feddfeb73d9e368aff0cd89c0652ea
rifall88 <rifaldistore9@gmail.com>
2025-08-03 10:12:05 +0700

    CCTV

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -35,7 +35,7 @@ export const validateUpdate = (req, res, next) => {
   const schema = Joi.object({
     username: Joi.string().required().min(3).max(50),
     role: Joi.string().required().valid("admin", "user").default("user"),
-    password: Joi.string().min(8),
+    password: Joi.string().min(8).required(),
   });
 
   const { error } = schema.validate(req.body, { abortEarly: false });
